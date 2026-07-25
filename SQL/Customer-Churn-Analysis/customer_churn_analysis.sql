0a9c847d6002accf7177be02863383cbba83e0cf
Sevinch Bultakova <sevinchboltakova03@gmail.com>
2026-07-25 08:39:36 +0300

    Add churn analysis by internet service

--- SQL/Customer-Churn-Analysis/customer_churn_analysis.sql
+++ SQL/Customer-Churn-Analysis/customer_churn_analysis.sql
@@ -71,3 +71,18 @@ SELECT
 FROM customers
 GROUP BY paymentmethod
 ORDER BY churn_rate DESC;
+-- =====================================
+-- 5. Churn Analysis by Internet Service
+-- =====================================
+
+SELECT
+    internetservice,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate
+FROM customers
+GROUP BY internetservice
+ORDER BY churn_rate DESC;
