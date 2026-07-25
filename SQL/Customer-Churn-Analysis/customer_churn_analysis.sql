4fc0e281ccc4b49c14ee333650c7840f460f4305
Sevinch Bultakova <sevinchboltakova03@gmail.com>
2026-07-25 08:34:59 +0300

    Add churn analysis by payment method

--- SQL/Customer-Churn-Analysis/customer_churn_analysis.sql
+++ SQL/Customer-Churn-Analysis/customer_churn_analysis.sql
@@ -56,3 +56,18 @@ SELECT
 FROM customers
 GROUP BY contract
 ORDER BY churn_rate DESC;
+-- =====================================
+-- 4. Churn Analysis by Payment Method
+-- =====================================
+
+SELECT
+    paymentmethod,
+    COUNT(*) AS total_customers,
+    SUM(CASE WHEN churn = 'Yes' THEN 1 ELSE 0 END) AS churned_customers,
+    ROUND(
+        SUM(CASE WHEN churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+        2
+    ) AS churn_rate
+FROM customers
+GROUP BY paymentmethod
+ORDER BY churn_rate DESC;
